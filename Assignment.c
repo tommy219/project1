0e1d8dbc6131d503fb74bd1bc0b6a34a9da180c8
tommy219 <48034892+tommy219@users.noreply.github.com>
2019-04-10 09:12:36 +1000

    Adding Menu and refining

--- Assignment.c
+++ Assignment.c
@@ -1,45 +1,100 @@
 #include <stdio.h>
 
-//Function prototype
+//Function prototypes
 
-void encryptionFunction(char *testStatement, int cipherKey);
+void rotationEncrypt(char *rotMessage, int cipherKey);                
+
+void rotationDecrypt(char *rotMessage, int cipherKey);
+
+void substituionEncrypt(char *subMessage, int cipherKey);
+
+void substitutionDecrypt(char *subMessage, int cipherKey);
 
 int main()
 {
-    char testStatement[1000];
+    char rotMessage[1000];
     int cipherKey;
-    scanf("%s", testStatement);                                            //Gets message to be tested from the 'input' file
+    
+    printf("Please select an option: \n");
+    printf("To encrypt by rotation, press A.\n");
+    printf("To decrypt by rotation, press B.\n");
+
+    switch(input) {
+        case ’A’: rotationEncrypt(rotMessage, cipherKey); break;
+        case ’B’: rotationDecrypt(rotMessage, cipherKey); break;
+        default: printf("Unknown option %c\nPlease enter A or B\n", input);
+    }
+    
+    
+    
+    
+    scanf("%s", rotMessage);                                            //Gets message to be tested from the 'input' file
     scanf("%d", &cipherKey);                                               //Gets the key for the cipher from the 'input' file
-    encryptionFunction(testStatement, cipherKey);                          //Calling the result of the encryption function
+    
 }
 
 //Function definition for encrypting the message
 
-void encryptionFunction(char *testStatement, int cipherKey)
+void rotationEncrypt(char *rotMessage, int cipherKey)
 {
     int i;
-    for(i = 0; testStatement[i] != '\0'; ++i)                              //Goes through each element of the string and changes its value
+    for(i = 0; rotMessage[i] != 0; ++i)                              //Goes through each element of the string and changes its value
     {
 
-        if(testStatement[i] >= 65 && testStatement[i] < 90)                //65 and 90 are ASCII values for 'A' and 'Z'
+        if(rotMessage[i] >= 65 && rotMessage[i] < 90)                //65 and 90 are ASCII values for 'A' and 'Z'
         {
-            testStatement[i] += cipherKey;                                 //Assigninging the individual character to its new value
+           rotMessage[i] += cipherKey;                                 //Assigninging the individual character to its new value
         }
-        else if(testStatement[i] == 90)                                    
+        else if(rotMessage[i] == 90)                                    
         {
-            testStatement[i] = (testStatement[i] - 90) + 64 + cipherKey;   //Loops to beginning of alphabet when going past 'Z'
+            rotMessage[i] = (rotMessage[i] - 90) + 64 + cipherKey;   //Loops to beginning of alphabet when going past 'Z'
         }
-        else if(testStatement[i] >= 97 && testStatement[i] < 122)          //97 and 122 are ASCII values for 'a' and 'z'
+        else if(rotMessage[i] >= 97 && rotMessage[i] < 122)          //97 and 122 are ASCII values for 'a' and 'z'
         {
-            testStatement[i] += cipherKey;                                 //Assigninging the individual character to its new value
+            rotMessage[i] -= 32;
+            rotMessage[i] += cipherKey;                                 //Assigninging the individual character to its new value
         }
-        else if(testStatement[i] == 122)                                   
+        else if(rotMessage[i] == 122)                                   
         {
-            testStatement[i] = (testStatement[i] - 122) + 96 + cipherKey;  //Loops to beginning of alphabet when going past 'z'
+            rotMessage[i] = (rotMessage[i] - 122) + 64 + cipherKey;  //Loops to beginning of alphabet when going past 'z'
         }
     }
-    printf("%s", testStatement);
-} 
+    printf("The encrypted message is: %s\n\n", rotMessage);
+}   
     
+    
+
+
+
+//Function Definition for decrypting the message
+
+
+
+void rotationDecrypt(char *rotMessage, int cipherKey)
+{
+    int i;
+    for(i = 0; rotMessage[i] != '\0'; ++i)                              //Goes through each element of the string and changes its value
+    {
+
+        if(rotMessage[i] >= 65 && rotMessage[i] < 90)                //65 and 90 are ASCII values for 'A' and 'Z'
+        {
+            rotMessage[i] -= cipherKey;                                 //Assigninging the individual character to its new value, which is correct letter
+        }
+        else if(rotMessage[i] == 90)                                    
+        {
+            rotMessage[i] = (rotMessage[i] - 90) + 64 - cipherKey;   //Loops to beginning of alphabet when going past 'Z'
+        }
+        else if(rotMessage[i] >= 97 && rotMessage[i] < 122)          //97 and 122 are ASCII values for 'a' and 'z'
+        {
+            rotMessage[i] -= cipherKey;                                 //Assigninging the individual character to its new value, which is correct letter
+        }
+        else if(rotMessage[i] == 122)                                   
+        {
+            rotMessage[i] = (rotMessage[i] - 122) + 96 - cipherKey;  //Loops to beginning of alphabet when going past 'z'
+        }
+    }
+    printf("The decrypted message is: %s", rotMessage);
+}   
+   
     
     
